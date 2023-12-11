519d9b9a8e6ea333c9217a55dc677ee1d62c1caf
LossaLos <122557035+LossaLos@users.noreply.github.com>
2023-12-11 23:46:31 +0100

    Add files via upload

--- Push_swap/sort_beyond5.c
+++ Push_swap/sort_beyond5.c
@@ -38,7 +38,8 @@
         push_b(stack_a, stack_b);
         num_moved++;
     }
-}*/
+}
+*/
 
 
 
@@ -46,14 +47,72 @@
 void    sort_stack_beyond_5(t_list **stack_a, t_list **stack_b, int stack_size)
 {
     //divide_stack(stack_a, stack_b, pivot, stack_size);
-    push_b(stack_a, stack_b);
-    int pivot = (*stack_b)->content;
+   
 
-    quicksort(stack_a, stack_a, pivot);
     printf("stack A : ");
     print_stack2(*stack_a);
     printf("stack B : ");
     print_stack2(*stack_b);
-    
-
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+static int	get_max_bits(t_list **stack)
+{
+	t_list	*head;
+	int		max;
+	int		max_bits;
+
+	head = *stack;
+	max = head->content;
+	max_bits = 0;
+	while (head)
+	{
+		if (head->content > max)
+			max = head->content;
+		head = head->next;
+	}
+	while ((max >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
+}
+
+void	sort_stack_beyond_5(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*head_a;
+	int		i;
+	int		j;
+	int		size;
+	int		max_bits;
+
+	i = 0;
+	head_a = *stack_a;
+	size = ft_lstsize(head_a);
+	max_bits = get_max_bits(stack_a);
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j++ < size)
+		{
+			head_a = *stack_a;
+			if (((head_a->content >> i) & 1) == 1)
+				rotate_a(stack_a);
+			else
+				push_b(stack_a, stack_b);
+		}
+		while (ft_lstsize(*stack_b) != 0)
+			push_a(stack_a, stack_b);
+		i++;
+	}
+    printf("stack A : ");
+    print_stack2(*stack_a);
+}*/
